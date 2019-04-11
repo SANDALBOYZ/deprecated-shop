56161bdf7a4e045170f972adba86c78b48046266
Ryan Chang <ryan@sandalboyz.com>
2019-04-10 20:34:44 -0700

    ssr

---
+++ src/pages/help.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Layout } from 'components/Layout'
+import withProvider from 'withProvider'
+
+const Help = () => {
+  return (
+    <Layout>
+      Help
+    </Layout>
+  )
+}
+
+export default withProvider(Help)
