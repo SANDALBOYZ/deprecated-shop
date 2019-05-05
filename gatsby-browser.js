c3c9984513f71484d002f230afcb6dd575f7951d
Ryan Chang <ryan@sandalboyz.com>
2019-05-04 19:57:03 -0700

    adds newline

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { ApolloProvider } from 'react-apollo'
 import client from 'api/client'
 
-console.log('Inside `gatsby-ssr.js`')
+console.log('\n\nInside `gatsby-ssr.js`')
 console.log(process.env.SHOPIFY_GRAPHQL_URI)
 console.log(process.env.SHOPIFY_ACCESS_TOKEN)
 
