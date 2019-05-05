9ef7dd8856582d50e52eb53c7769a7ff66487647
Ryan Chang <ryan@sandalboyz.com>
2019-05-04 20:28:17 -0700

    stuff

--- src/api/client.js
+++ src/api/client.js
@@ -2,9 +2,13 @@
 import ApolloClient from 'apollo-boost'
 import fetch from 'isomorphic-fetch'
 
-console.log('\n\nInside `api/client.js`')
+console.log('================================')
+console.log('================================')
+console.log('Inside `api/client.js`')
 console.log(process.env.SHOPIFY_GRAPHQL_URI)
 console.log(process.env.SHOPIFY_ACCESS_TOKEN)
+console.log(process.env.GATSBY_SHOPIFY_GRAPHQL_URI)
+console.log(process.env.GATSBY_SHOPIFY_ACCESS_TOKEN)
 
 const client = new ApolloClient({
   uri: process.env.SHOPIFY_GRAPHQL_URI,
